12d319e95175f90e69eae4b4555e0a14f94df9b7
Erik Dasque <ed@Erik-Dasques-MacBook-Pro.local>
2010-03-07 21:44:59 -0500

    added script to detect whether we should keep a backup or not

---
+++ ec2-describe-backup.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * 
+ *
+ * @author Erik Dasque
+ * @version $Id$
+ * @copyright __MyCompanyName__, 7 March, 2010
+ * @package default
+ **/
+date_default_timezone_set('UTC');
+
+$now = time();
+$older_than = $now - 7 * 24 * 60 * 60;
+$older_than_month = $now - 30 * 24 * 60 * 60;
+
+
+// echo "Older than month: ".date('F d, Y',$older_than_month)."\n";
+//echo "Older than week: ".date('F d, Y',$older_than)."\n";
+
+for ($date=$older_than_month-50*24*60*60; $date < $now; $date=$date+24*60*60) { 
+	// echo "-> ".date('F d, Y',$date)."\n";
+	keepSnapShot(date('F d, Y',$date));
+	 
+}
+
+//echo keepSnapShot("3/05/10");
+//echo keepSnapShot("3/1/10");
+//echo keepSnapShot("3/7/10");
+
+
+function keepSnapShot($creation_date)
+{
+	$now = time();
+	$older_than = $now - 7 * 24 * 60 * 60;
+	$older_than_month = $now - 30 * 24 * 60 * 60;
+	
+	
+	// echo strtotime($creation_date);
+	$ts = strtotime($creation_date);
+//	echo 'Day of month: '.date("d",$ts)."\n";
+//	echo 'Day of week: '.date("w",$ts)."\n";
+	echo date('F d, Y',$ts)."\t";
+	
+	if (date("d",$ts)==1) { echo "1st of month\tKEEP\n" ; return true; }
+	if ((date("w",$ts)==0) && $ts>$older_than_month) { echo "Recent Sunday\tKEEP\n" ; return true; } // What if too old?
+	if ((date("w",$ts)==0) && $ts<=$older_than_month) { echo "Old Sunday\tDELETE\n" ; return false; } // What if too old?
+	if ($ts>=$older_than) { echo "Recent backup\tKEEP\n" ; return true; } // What if too old?
+	if ($ts<$older_than) { echo "Old backup\tDELETE\n" ; return false; } // What if too old?
+	
+	echo "Unknown condition on ".date('F d, Y',$ts)."\n"; exit(0);
+	return false;
+}
+
+?>
