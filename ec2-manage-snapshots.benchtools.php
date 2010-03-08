fd37432923aadfe4c1ae2161029ff40d9565c9f8
Erik Dasque <ed@Erik-Dasques-MacBook-Pro.local>
2010-03-08 14:13:22 +0100

    removed my keys from the distribution file. Created a copy for BT

--- ec2-manage-snapshots.benchtools.php
+++ ec2-manage-snapshots.benchtools.php
@@ -10,9 +10,6 @@
  **/
 
 // Your EC2 credentials
-define('AWS_ACCESS_KEY_ID', ''); 
-define('AWS_SECRET_ACCESS_KEY', '');
-
 define('AWS_ACCESS_KEY_ID', 'AKIAIMQDJK3HJA6I4VYA');
 define('AWS_SECRET_ACCESS_KEY', 'Um5L7z8l5N/EqsmV1c2eVp8/ekWFyPyh5SyYwM7w'); 
 
